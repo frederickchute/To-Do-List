997b8f8f0fe2ec7bc6c5f010de77f84750d3d1c7
Frederick Chute <frederickchute@gmail.com>
2019-06-21 15:54:23 -0400

    Add ability to click and hide error messages as well as using key presses

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,9 +1,6 @@
-$('input[name="text"]').on('keypress', function() {
-    $('.has-error').hide()
-});
-
-var initialize = function () {
-    $('input[name="text"]').on('keypress', function() {
+window.Superlists = {};
+window.Superlists.initialize = function () {
+  $('input[name="text"]').on('keypress click', function() {
     $('.has-error').hide()
 });
 };
