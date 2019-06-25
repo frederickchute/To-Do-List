62f62dffedcca07667d570eb8801c6aa48e63144
Frederick Chute <frederickchute@gmail.com>
2019-06-25 14:54:17 -0400

    Resolved another syntax error in session_tools

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -18,7 +18,7 @@ def _get_server_env_vars(host):
 
 
 def create_session_on_server(host, email):
-    manage_dot_py = _get_manage_dot_py
+    manage_dot_py = _get_manage_dot_py(host)
     with settings(host_string=f'fred@{host}'):
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
