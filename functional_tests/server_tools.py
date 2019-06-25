93c1dd41bb9accc7915ce080acd83ea224930774
Frederick Chute <frederickchute@gmail.com>
2019-06-25 14:35:41 -0400

    syntax error in server tools

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -20,7 +20,7 @@ def _get_server_env_vars(host):
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py
     with settings(host_string=f'fred@{host}'):
-        env_vars = _get_server_env_vars
+        env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
             session_key = run(f'{manage_dot_py} create_session {email}')
             return session_key.strip()
